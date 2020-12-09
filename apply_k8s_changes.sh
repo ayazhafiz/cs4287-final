3d5eab47a90ad0156dee906bf66c0f69b94b558d
Kevin Jin <kevin.jin@vanderbilt.edu>
2020-12-09 13:06:17 -0700

    try k8s cd 3

--- apply_k8s_changes.sh
+++ apply_k8s_changes.sh
@@ -6,7 +6,7 @@ git diff --name-only HEAD^ HEAD > changed_files.txt
 while IFS= read -r file
 do
   echo "Changed file: $file"
-  if [[ $file == kubernetes/$lang/* && $file =~ ".yaml" ]]; then
+  if [[ $file == kubernetes/* && $file =~ ".yaml" ]]; then
     echo "Applying k8s changes in $file"
     kubectl apply -f $file
   fi
